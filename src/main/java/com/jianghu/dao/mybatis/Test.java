252ce3c24cffe1a0e05e0b7d0b37786dc848c7af
jiong-long <jiong_long@163.com>
2018-09-19 00:47:45 +0800

    集成MyBatis

---
+++ src/main/java/com/jianghu/dao/mybatis/Test.java
@@ -0,0 +1,30 @@
+package com.jianghu.dao.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.jianghu.core.MyBatisDB;
+import com.jianghu.domain.basic.User;
+
+public class Test {
+	public static void main(String[] args) {
+		selectUserById();
+	}
+
+	/**
+	 * 根据id查询用户
+	 */
+	private static void selectUserById() {
+		SqlSession session = MyBatisDB.getSession();
+		try {
+			User user = session.selectOne("com.jianghu.dao.mybatis.xml.UserMapper", "1");
+			System.out.println(user.toString());
+
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.rollback();
+		} finally {
+			session.close();
+		}
+	}
+}
