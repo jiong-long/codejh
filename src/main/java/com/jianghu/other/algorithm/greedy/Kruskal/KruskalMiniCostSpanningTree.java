788b652c7599a410607cde02e1647d80b4343863
jiong-long <jiong_long@163.com>
2018-09-23 19:08:09 +0800

    完善克鲁斯卡尔算法

--- src/main/java/com/jianghu/other/algorithm/greedy/Kruskal/KruskalMiniCostSpanningTree.java
+++ src/main/java/com/jianghu/other/algorithm/greedy/Kruskal/KruskalMiniCostSpanningTree.java
@@ -18,37 +18,15 @@ public class KruskalMiniCostSpanningTree {
 	// 边集合,按照权重升序排列
 	private List<Edge> edgesList = new ArrayList<Edge>();
 
-	// 最大Int整数,表示无穷大
-	private int MAX_VALUE = Integer.MAX_VALUE;
-
 	// 定义节点
 	private String nodes[] = { "vo", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8" };
 
-	// 定义地图变量 [节点编号][改节点到其他节点的权重]
-	private int map[][] = new int[9][9];
-
 	public KruskalMiniCostSpanningTree() {
-		this.initMap();
 		this.initEdages();
-	}
-
-	// 初始化地图
-	public void initMap() {
-		this.map[0] = new int[] { 0, 10, MAX_VALUE, MAX_VALUE, MAX_VALUE, 11, MAX_VALUE, MAX_VALUE, MAX_VALUE };
-		this.map[1] = new int[] { 10, 0, 18, MAX_VALUE, MAX_VALUE, MAX_VALUE, 16, MAX_VALUE, 12 };
-		this.map[2] = new int[] { MAX_VALUE, MAX_VALUE, 0, 22, MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE, 8 };
-		this.map[3] = new int[] { MAX_VALUE, MAX_VALUE, 22, 0, 20, MAX_VALUE, MAX_VALUE, 16, 21 };
-		this.map[4] = new int[] { MAX_VALUE, MAX_VALUE, MAX_VALUE, 20, 0, 26, MAX_VALUE, 7, MAX_VALUE };
-		this.map[5] = new int[] { 11, MAX_VALUE, MAX_VALUE, MAX_VALUE, 26, 0, 17, MAX_VALUE, MAX_VALUE };
-		this.map[6] = new int[] { MAX_VALUE, 16, MAX_VALUE, MAX_VALUE, MAX_VALUE, 17, 0, 19, MAX_VALUE };
-		this.map[7] = new int[] { MAX_VALUE, MAX_VALUE, MAX_VALUE, 16, 7, MAX_VALUE, 19, 0, MAX_VALUE };
-		this.map[8] = new int[] { MAX_VALUE, 12, 8, 21, MAX_VALUE, MAX_VALUE, MAX_VALUE, MAX_VALUE, 0 };
-
 	}
 
 	// 初始化边和权的list集合
 	public void initEdages() {
-
 		// 必须按照【权重升序】排列
 		Edge v0 = new Edge(4, 7, 7);
 		Edge v1 = new Edge(2, 8, 8);
