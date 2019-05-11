46a3bce43ff024e412f67fc90210d605ffbb946a
jiong-long <jiong_long@163.com>
2019-05-11 14:54:39 +0800

    调用saop协议接口

---
+++ src/main/java/com/jianghu/core/func/callSoap/Contacts.java
@@ -0,0 +1,160 @@
+/**
+ * Contacts.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.jianghu.core.func.callSoap;
+
+public class Contacts  implements java.io.Serializable {
+	private static final long serialVersionUID = 8144072007472176161L;
+
+	private java.lang.String name1;
+
+    private java.lang.String telf1;
+
+    public Contacts() {
+    }
+
+    public Contacts(
+           java.lang.String name1,
+           java.lang.String telf1) {
+           this.name1 = name1;
+           this.telf1 = telf1;
+    }
+
+
+    /**
+     * Gets the name1 value for this Contacts.
+     * 
+     * @return name1
+     */
+    public java.lang.String getName1() {
+        return name1;
+    }
+
+
+    /**
+     * Sets the name1 value for this Contacts.
+     * 
+     * @param name1
+     */
+    public void setName1(java.lang.String name1) {
+        this.name1 = name1;
+    }
+
+
+    /**
+     * Gets the telf1 value for this Contacts.
+     * 
+     * @return telf1
+     */
+    public java.lang.String getTelf1() {
+        return telf1;
+    }
+
+
+    /**
+     * Sets the telf1 value for this Contacts.
+     * 
+     * @param telf1
+     */
+    public void setTelf1(java.lang.String telf1) {
+        this.telf1 = telf1;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof Contacts)) return false;
+        Contacts other = (Contacts) obj;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.name1==null && other.getName1()==null) || 
+             (this.name1!=null &&
+              this.name1.equals(other.getName1()))) &&
+            ((this.telf1==null && other.getTelf1()==null) || 
+             (this.telf1!=null &&
+              this.telf1.equals(other.getTelf1())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getName1() != null) {
+            _hashCode += getName1().hashCode();
+        }
+        if (getTelf1() != null) {
+            _hashCode += getTelf1().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(Contacts.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("http://customer.ec.business.webservice.liems.luculent.net/", "contacts"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("name1");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "name1"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("telf1");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "telf1"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    @SuppressWarnings("rawtypes")
+	public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    @SuppressWarnings("rawtypes")
+	public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
---
+++ src/main/java/com/jianghu/core/func/callSoap/CustomerImplServiceLocator.java
@@ -0,0 +1,147 @@
+/**
+ * CustomerImplServiceLocator.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.jianghu.core.func.callSoap;
+
+public class CustomerImplServiceLocator extends org.apache.axis.client.Service implements CustomerImplService {
+	private static final long serialVersionUID = 1L;
+
+	public CustomerImplServiceLocator() {
+    }
+
+
+    public CustomerImplServiceLocator(org.apache.axis.EngineConfiguration config) {
+        super(config);
+    }
+
+    public CustomerImplServiceLocator(java.lang.String wsdlLoc, javax.xml.namespace.QName sName) throws javax.xml.rpc.ServiceException {
+        super(wsdlLoc, sName);
+    }
+
+    // Use to get a proxy class for CustomerImplPort
+    private java.lang.String CustomerImplPort_address = "http://localhost:8080/Liems/webservice/customerImpl";
+
+    public java.lang.String getCustomerImplPortAddress() {
+        return CustomerImplPort_address;
+    }
+
+    // The WSDD service name defaults to the port name.
+    private java.lang.String CustomerImplPortWSDDServiceName = "CustomerImplPort";
+
+    public java.lang.String getCustomerImplPortWSDDServiceName() {
+        return CustomerImplPortWSDDServiceName;
+    }
+
+    public void setCustomerImplPortWSDDServiceName(java.lang.String name) {
+        CustomerImplPortWSDDServiceName = name;
+    }
+
+    public ICustomer getCustomerImplPort() throws javax.xml.rpc.ServiceException {
+       java.net.URL endpoint;
+        try {
+            endpoint = new java.net.URL(CustomerImplPort_address);
+        }
+        catch (java.net.MalformedURLException e) {
+            throw new javax.xml.rpc.ServiceException(e);
+        }
+        return getCustomerImplPort(endpoint);
+    }
+
+    public ICustomer getCustomerImplPort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException {
+        try {
+            CustomerImplServiceSoapBindingStub _stub = new CustomerImplServiceSoapBindingStub(portAddress, this);
+            _stub.setPortName(getCustomerImplPortWSDDServiceName());
+            return _stub;
+        }
+        catch (org.apache.axis.AxisFault e) {
+            return null;
+        }
+    }
+
+    public void setCustomerImplPortEndpointAddress(java.lang.String address) {
+        CustomerImplPort_address = address;
+    }
+
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    @SuppressWarnings("rawtypes")
+	public java.rmi.Remote getPort(Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        try {
+            if (ICustomer.class.isAssignableFrom(serviceEndpointInterface)) {
+                CustomerImplServiceSoapBindingStub _stub = new CustomerImplServiceSoapBindingStub(new java.net.URL(CustomerImplPort_address), this);
+                _stub.setPortName(getCustomerImplPortWSDDServiceName());
+                return _stub;
+            }
+        }
+        catch (java.lang.Throwable t) {
+            throw new javax.xml.rpc.ServiceException(t);
+        }
+        throw new javax.xml.rpc.ServiceException("There is no stub implementation for the interface:  " + (serviceEndpointInterface == null ? "null" : serviceEndpointInterface.getName()));
+    }
+
+    /**
+     * For the given interface, get the stub implementation.
+     * If this service has no port for the given interface,
+     * then ServiceException is thrown.
+     */
+    @SuppressWarnings("rawtypes")
+	public java.rmi.Remote getPort(javax.xml.namespace.QName portName, Class serviceEndpointInterface) throws javax.xml.rpc.ServiceException {
+        if (portName == null) {
+            return getPort(serviceEndpointInterface);
+        }
+        java.lang.String inputPortName = portName.getLocalPart();
+        if ("CustomerImplPort".equals(inputPortName)) {
+            return getCustomerImplPort();
+        }
+        else  {
+            java.rmi.Remote _stub = getPort(serviceEndpointInterface);
+            ((org.apache.axis.client.Stub) _stub).setPortName(portName);
+            return _stub;
+        }
+    }
+
+    public javax.xml.namespace.QName getServiceName() {
+        return new javax.xml.namespace.QName("http://customer.ec.business.webservice.liems.luculent.net/", "CustomerImplService");
+    }
+
+    @SuppressWarnings("rawtypes")
+	private java.util.HashSet ports = null;
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public java.util.Iterator getPorts() {
+        if (ports == null) {
+            ports = new java.util.HashSet();
+            ports.add(new javax.xml.namespace.QName("http://customer.ec.business.webservice.liems.luculent.net/", "CustomerImplPort"));
+        }
+        return ports.iterator();
+    }
+
+    /**
+    * Set the endpoint address for the specified port name.
+    */
+    public void setEndpointAddress(java.lang.String portName, java.lang.String address) throws javax.xml.rpc.ServiceException {
+        
+if ("CustomerImplPort".equals(portName)) {
+            setCustomerImplPortEndpointAddress(address);
+        }
+        else 
+{ // Unknown Port Name
+            throw new javax.xml.rpc.ServiceException(" Cannot set Endpoint Address for Unknown Port" + portName);
+        }
+    }
+
+    /**
+    * Set the endpoint address for the specified port name.
+    */
+    public void setEndpointAddress(javax.xml.namespace.QName portName, java.lang.String address) throws javax.xml.rpc.ServiceException {
+        setEndpointAddress(portName.getLocalPart(), address);
+    }
+
+}
