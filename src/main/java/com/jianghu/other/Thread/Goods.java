22e3488ffa4343e4f524fee5a4b175cf6ae5cc13
jiong-long <jiong_long@163.com>
2020-07-31 23:13:47 +0800

    生产者、消费者模型

---
+++ src/main/java/com/jianghu/other/Thread/Goods.java
@@ -0,0 +1,24 @@
+package com.jianghu.other.Thread;
+
+/**
+ * 商品
+ * @author wangjinlong
+ * @datetime Jul 31, 2020 10:38:11 PM
+ *
+ */
+public class Goods {
+	private int goodId;
+
+	public Goods(int goodId) {
+		super();
+		this.goodId = goodId;
+	}
+
+	public int getGoodId() {
+		return goodId;
+	}
+
+	public void setGoodId(int goodId) {
+		this.goodId = goodId;
+	}
+}
---
+++ src/main/java/com/jianghu/other/Thread/GoodsPool.java
@@ -0,0 +1,41 @@
+package com.jianghu.other.Thread;
+
+/**
+ * 缓冲区
+ * @author wangjinlong
+ * @datetime Jul 31, 2020 10:37:46 PM
+ *
+ */
+public class GoodsPool {
+	private int currId = 0;
+    Goods[] goodPool = new Goods[10];
+    
+    public synchronized void push(Goods goods) {
+    	while(currId == goodPool.length) {
+    		try {
+    			// wait后，线程会将持有的锁释放，进入阻塞状态；
+    			// 这样其它需要锁的线程就可以获得锁；
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	// 唤醒在当前对象等待池中等待的第一个线程。
+        // notifyAll叫醒所有在当前对象等待池中等待的所有线程。
+    	this.notify();
+    	goodPool[currId++] = goods;
+    }
+    
+    public synchronized Goods pop() {
+    	while(currId == 0) {
+    		try {
+				this.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+    	}
+    	this.notify();
+    	return goodPool[--currId];
+    }
+}
