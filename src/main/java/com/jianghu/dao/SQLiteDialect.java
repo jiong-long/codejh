30cd0c7bf97c2baf5e192c264dc700b8071c8ea0
jiong-long <jiong_long@163.com>
2020-12-18 00:07:12 +0800

    修改jar包位置

--- src/main/java/com/jianghu/dao/SQLiteDialect.java
+++ src/main/java/com/jianghu/dao/SQLiteDialect.java
@@ -1,13 +1,13 @@
 package com.jianghu.dao;
 
-import java.sql.Types;
-
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
 import org.hibernate.type.IntegerType;
 import org.hibernate.type.StringType;
+
+import java.sql.Types;
 
 /**
  * hibernate没有sqlite方言，需要自己写
@@ -46,7 +46,6 @@ public class SQLiteDialect extends Dialect {
 		registerFunction("substring", new StandardSQLFunction("substr", StringType.INSTANCE));
 	}
 
-	@Override
 	public boolean supportsIdentityColumns() {
 		return true;
 	}
@@ -56,18 +55,15 @@ public class SQLiteDialect extends Dialect {
 	 * specify in NHibernate dialect }
 	 */
 
-	@Override
 	public boolean hasDataTypeInIdentityColumn() {
 		return false; // As specify in NHibernate dialect
 	}
 
-	@Override
 	public String getIdentityColumnString() {
 		// return "integer primary key autoincrement";
 		return "integer";
 	}
 
-	@Override
 	public String getIdentitySelectString() {
 		return "select last_insert_rowid()";
 	}
@@ -83,17 +79,14 @@ public class SQLiteDialect extends Dialect {
 				.toString();
 	}
 
-	@Override
 	public boolean supportsTemporaryTables() {
 		return true;
 	}
 
-	@Override
 	public String getCreateTemporaryTableString() {
 		return "create temporary table if not exists";
 	}
 
-	@Override
 	public boolean dropTemporaryTableAfterUse() {
 		return false;
 	}
